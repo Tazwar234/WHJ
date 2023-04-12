3b59e9f530fec4a3dbfde6cb492cb058986498c4
Tazwar234 <106187837+Tazwar234@users.noreply.github.com>
2023-04-11 19:37:43 -0500

    Update StarMap.js

--- C89/project_solution/screens/StarMap.js
+++ C89/project_solution/screens/StarMap.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+npm install 'react-native-webview'
+import {webView} from 'react-native-webview'
+const {longitude, latitude } = this.state;
 
 export default class StarMapScreen extends Component {
+    <WebView
+       scalesPageToFit={true}
+       source = {{ uri:path }}
+       styles = {{ marginTop : 20, marginBottom : 20, }}
+/>
+<TextInput
+ style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+ placeholder="Enter your latitude"
+ placeHolderTextColor = "#ffff"
+ onChangeText={(text)}=>{
+     this.setState({
+         latitude: text
+     })
+
+/>
     render() {
         return (
             <View
